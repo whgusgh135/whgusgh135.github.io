a96bd94cd44b4f5b154aa95751e671edaf96007f
whgusgh135 <33997616+whgusgh135@users.noreply.github.com>
2022-05-11 17:03:40 +1200

    Update waapideck.js

--- waapideck.js
+++ waapideck.js
@@ -1,40 +1,40 @@
-const menuButton = document.querySelector("#menu-toggle");
-        const menuIcon = document.querySelector(".header-navbar__mobile-menu-button")
-        const mobileHeader = document.querySelector(".mobile-header__menu");
-        const header = document.querySelector(".header-navbar");
+let menuButton = document.querySelector("#menu-toggle");
+let menuIcon = document.querySelector(".header-navbar__mobile-menu-button")
+let mobileHeader = document.querySelector(".mobile-header__menu");
+let header = document.querySelector(".header-navbar");
 
-        menuButton.addEventListener("click", () => {
-            mobileHeader.classList.toggle("mobile-header__menu--open");
-            header.classList.toggle("fixed-position");
-            menuIcon.classList.toggle("fixed-position");
-        });
+menuButton.addEventListener("click", () => {
+    mobileHeader.classList.toggle("mobile-header__menu--open");
+    header.classList.toggle("fixed-position");
+    menuIcon.classList.toggle("fixed-position");
+});
 
-        const howButton = document.querySelector("#howButton");
-        const howDropdown = document.querySelector("#howDropdown");
+let howButton = document.querySelector("#howButton");
+let howDropdown = document.querySelector("#howDropdown");
 
-        howButton.addEventListener("click", () => {
-            howButton.classList.toggle("mobile-header__nav-item-link--grey-background");
-            howDropdown.classList.toggle("mobile-header__dropdown--open");
-            whoDropdown.classList.remove("mobile-header__dropdown--open");
-            companyDropdown.classList.remove("mobile-header__dropdown--open");
-        });
+howButton.addEventListener("click", () => {
+    howButton.classList.toggle("mobile-header__nav-item-link--grey-background");
+    howDropdown.classList.toggle("mobile-header__dropdown--open");
+    whoDropdown.classList.remove("mobile-header__dropdown--open");
+    companyDropdown.classList.remove("mobile-header__dropdown--open");
+});
 
-        const whoButton = document.querySelector("#whoButton");
-        const whoDropdown = document.querySelector("#whoDropdown");
+let whoButton = document.querySelector("#whoButton");
+let whoDropdown = document.querySelector("#whoDropdown");
 
-        whoButton.addEventListener("click", () => {
-            whoButton.classList.toggle("mobile-header__nav-item-link--grey-background");
-            whoDropdown.classList.toggle("mobile-header__dropdown--open");
-            howDropdown.classList.remove("mobile-header__dropdown--open");
-            companyDropdown.classList.remove("mobile-header__dropdown--open");
-        });
+whoButton.addEventListener("click", () => {
+    whoButton.classList.toggle("mobile-header__nav-item-link--grey-background");
+    whoDropdown.classList.toggle("mobile-header__dropdown--open");
+    howDropdown.classList.remove("mobile-header__dropdown--open");
+    companyDropdown.classList.remove("mobile-header__dropdown--open");
+});
 
-        const companyButton = document.querySelector("#companyButton");
-        const companyDropdown = document.querySelector("#companyDropdown");
+let companyButton = document.querySelector("#companyButton");
+let companyDropdown = document.querySelector("#companyDropdown");
 
-        companyButton.addEventListener("click", () => {
-            companyButton.classList.toggle("mobile-header__nav-item-link--grey-background");
-            companyDropdown.classList.toggle("mobile-header__dropdown--open");
-            howDropdown.classList.remove("mobile-header__dropdown--open");
-            whoDropdown.classList.remove("mobile-header__dropdown--open");
-        });
+companyButton.addEventListener("click", () => {
+    companyButton.classList.toggle("mobile-header__nav-item-link--grey-background");
+    companyDropdown.classList.toggle("mobile-header__dropdown--open");
+    howDropdown.classList.remove("mobile-header__dropdown--open");
+    whoDropdown.classList.remove("mobile-header__dropdown--open");
+});
