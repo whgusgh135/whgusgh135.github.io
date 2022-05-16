eb20e2677ce9e482265069c6dac2faf8b5412a49
whgusgh135 <33997616+whgusgh135@users.noreply.github.com>
2022-05-16 16:09:19 +1200

    Update waapideck.js

--- waapideck.js
+++ waapideck.js
@@ -1,26 +1,40 @@
-document.querySelector("#mobile-header-dropdown-toggle").addEventListener("click", () => {
-    document.querySelector(".mobile-header__menu").classList.toggle("mobile-header__menu--open");
-    document.querySelector(".header-navbar").classList.toggle("fixed-position");
-    document.querySelector(".header-navbar__mobile-menu-button").classList.toggle("fixed-position");
+let menuButton = document.querySelector("#mobile-header-dropdown-toggle");
+let menuIcon = document.querySelector(".header-navbar__mobile-menu-button")
+let mobileHeader = document.querySelector(".mobile-header__menu");
+let header = document.querySelector(".header-navbar");
+
+menuButton.addEventListener("click", () => {
+    mobileHeader.classList.toggle("mobile-header__menu--open");
+    header.classList.toggle("fixed-position");
+    menuIcon.classList.toggle("fixed-position");
 });
 
-document.querySelector("#howButton").addEventListener("click", () => {
-    document.querySelector("#howButton").classList.toggle("mobile-header__nav-item-link--grey-background");
-    document.querySelector("#howDropdown").classList.toggle("mobile-header__dropdown--open");
-    document.querySelector("#whoDropdown").classList.remove("mobile-header__dropdown--open");
-    document.querySelector("#companyDropdown").classList.remove("mobile-header__dropdown--open");
+let howButton = document.querySelector("#howButton");
+let howDropdown = document.querySelector("#howDropdown");
+
+howButton.addEventListener("click", () => {
+    howButton.classList.toggle("mobile-header__nav-item-link--grey-background");
+    howDropdown.classList.toggle("mobile-header__dropdown--open");
+    whoDropdown.classList.remove("mobile-header__dropdown--open");
+    companyDropdown.classList.remove("mobile-header__dropdown--open");
 });
 
-document.querySelector("#whoButton").addEventListener("click", () => {
-    document.querySelector("#whoButton").classList.toggle("mobile-header__nav-item-link--grey-background");
-    document.querySelector("#whoDropdown").classList.toggle("mobile-header__dropdown--open");
-    document.querySelector("#howDropdown").classList.remove("mobile-header__dropdown--open");
-    document.querySelector("#companyDropdown").classList.remove("mobile-header__dropdown--open");
+let whoButton = document.querySelector("#whoButton");
+let whoDropdown = document.querySelector("#whoDropdown");
+
+whoButton.addEventListener("click", () => {
+    whoButton.classList.toggle("mobile-header__nav-item-link--grey-background");
+    whoDropdown.classList.toggle("mobile-header__dropdown--open");
+    howDropdown.classList.remove("mobile-header__dropdown--open");
+    companyDropdown.classList.remove("mobile-header__dropdown--open");
 });
 
-document.querySelector("#companyButton").addEventListener("click", () => {
-    document.querySelector("#companyButton").classList.toggle("mobile-header__nav-item-link--grey-background");
-    document.querySelector("#companyDropdown").classList.toggle("mobile-header__dropdown--open");
-    document.querySelector("#howDropdown").classList.remove("mobile-header__dropdown--open");
-    document.querySelector("#whoDropdown").classList.remove("mobile-header__dropdown--open");
+let companyButton = document.querySelector("#companyButton");
+let companyDropdown = document.querySelector("#companyDropdown");
+
+companyButton.addEventListener("click", () => {
+    companyButton.classList.toggle("mobile-header__nav-item-link--grey-background");
+    companyDropdown.classList.toggle("mobile-header__dropdown--open");
+    howDropdown.classList.remove("mobile-header__dropdown--open");
+    whoDropdown.classList.remove("mobile-header__dropdown--open");
 });
